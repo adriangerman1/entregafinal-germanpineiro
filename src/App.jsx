31983efd9a9c192d8edcbf91ef44df21e96de2f4
german piñeiro <germanadrian1@hotmail.com>
2022-10-20 15:26:20 -0300

    entregafinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import './App.css';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/Navbar/Navbar';
-import Samsung from './pages/Samsung';
-import Motorola from './pages/Motorola';
-import Iphone from './pages/Iphone';
-import Xiaomi from './pages/Xiaomi';
 import Footer from './components/Footer/Footer';
 import CardContext from './context/CardContext';
 import ItemListContainer from './container/ItemListContainer';
@@ -13,6 +9,7 @@ import Inicio from './container/Inicio';
 import CardWidget from './components/Navbar/CardWidget';
 import ContatacForm from './container/ContactForm';
 import ItemByid from './container/ItemByid';
+import CategoryPhones from './pages/CategoryPhones';
 
 
 function App() {
@@ -27,10 +24,7 @@ function App() {
           <Route path="/CardWidget" element={<CardWidget/>}/>
           <Route path="/galeria" element={<ItemListContainer/>}/>
           <Route path="/Item/:id" element={<ItemDetailContainer/>}/>
-          <Route path="/Category/Samsung" element={<Samsung/>}/>
-          <Route path="/Category/Motorola" element={<Motorola/>}/>
-          <Route path="//Category/Xiaomi" element={<Xiaomi/>}/>
-          <Route path="/Category/Iphone" element={<Iphone/>}/>
+          <Route path="/Category/:idCategory" element={<CategoryPhones/>}/>
           <Route path="/Form" element={<ContatacForm/>}/>
           <Route path="/ConsultaCompra" element={<ItemByid/>}/>
         </Routes>
--- src/pages/CategoryPhones.jsx
+++ src/pages/CategoryPhones.jsx
@@ -1,17 +1,18 @@
-import {useState,useEffect } from "react"
-import Item from "../container/Item"
 import {getFirestore,collection,getDocs,query,where}   from 'firebase/firestore'
+import { useState,useEffect,} from "react"
+import { useParams } from 'react-router-dom';
+import Item from "../container/Item"
 
-const Motorola = () =>{
+const CategoryPhones = () => {
+
+    let productId = useParams();
 
     const [product, setProduct] = useState([])
 
-    const Category = 'Motorola'
-    
         useEffect(() =>{
             const db = getFirestore()
             const items = collection(db, 'items')
-            const q = query(items, where('trademark', '==', Category))
+            const q = query(items, where('trademark', '==', productId.idCategory))
             getDocs(q).then((snapshot)  => {
                 const docs = snapshot.docs.map((doc)=> ({
                     id: doc.id,
@@ -19,22 +20,24 @@ const Motorola = () =>{
                 }))
                 setProduct(docs)
             })
-        },[Category])
+        },[productId])
+        
     
     return (
 
         <div className="container-fluid" >
             <div className="row">
-                {product.map((products) => ( 
+                {product.map((products) => (
 
                     <div className="col-md-3">
                         <Item product={products}/>
-                    </div>   
+                    </div>
                     
                 ))}
             </div>
         </div>
     )
+
 }
 
-export default Motorola
+export default CategoryPhones
